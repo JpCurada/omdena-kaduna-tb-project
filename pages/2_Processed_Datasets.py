fb0b7eb4ddfb1797c2e943984cad730c7c064479
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2024-01-26 12:22:56 +0800

    Update 2_Processed_Datasets.py

--- pages/2_Processed_Datasets.py
+++ pages/2_Processed_Datasets.py
@@ -22,7 +22,7 @@ st.subheader('Block 1a: Detailed Activities of Presumptive PTB Cases', divider='
 st.caption("Data from 2019 to 2023")
 
 
-block1a_complete = pd.read_csv("datasets/block1a_19_to_23_complete.csv")
+block1a_complete = pd.read_csv("datasets/block1a_19_to_23_complete.csv", index_col=0)
 converted_block1a = convert_df(block1a_complete)
 
 st.download_button(
@@ -35,7 +35,7 @@ st.download_button(
 st.subheader('Block 2a: Comprehensive Breakdown of All TB Cases', divider='grey')
 st.caption("Data from 2019 to 2023")
 
-block2a_complete = pd.read_csv("datasets/block2a_19_to_23_complete.csv")
+block2a_complete = pd.read_csv("datasets/block2a_19_to_23_complete.csv", index_col=0)
 converted_block2a = convert_df(block2a_complete)
 
 st.download_button(
@@ -48,7 +48,7 @@ st.download_button(
 st.subheader('Block 2b: Demographic Breakdown of All TB Cases (by Sex and Age Group)', divider='grey')
 st.caption("Data from 2019 to 2023")
 
-block2b_complete = pd.read_csv("datasets/block2b_19_to_23_complete.csv")
+block2b_complete = pd.read_csv("datasets/block2b_19_to_23_complete.csv", index_col=0)
 converted_block2b = convert_df(block2b_complete)
 
 st.download_button(
@@ -61,7 +61,7 @@ st.download_button(
 st.subheader('Block 2c: Demographic Breakdown of New and Relapse TB Cases (by Sex and Age Group)', divider='grey')
 st.caption("Data from 2019 to 2023")
 
-block2c_complete = pd.read_csv("datasets/block2c_19_to_23_complete.csv")
+block2c_complete = pd.read_csv("datasets/block2c_19_to_23_complete.csv", index_col=0)
 converted_block2c = convert_df(block2c_complete)
 
 st.download_button(
@@ -74,7 +74,7 @@ st.download_button(
 st.subheader('Block 2d: Demographic Breakdown of HIV-Positive TB Cases (by Sex and Age Group)', divider='grey')
 st.caption("Data from 2019 to 2023")
 
-block2d_complete = pd.read_csv("datasets/block2d_19_to_23_complete.csv")
+block2d_complete = pd.read_csv("datasets/block2d_19_to_23_complete.csv", index_col=0)
 converted_block2d = convert_df(block2d_complete)
 
 st.download_button(
