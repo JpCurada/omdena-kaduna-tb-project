02882b07f866e6e8f5204d732c31e09a67c8592e
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2024-01-29 23:08:39 +0800

    Delete pages/4_Reports.py

--- pages/4_Reports.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from visuals import im
-
-st.set_page_config(page_title="Kaduna TB Reports", 
-                   page_icon=im, 
-                   layout="wide", 
-                   initial_sidebar_state="auto", 
-                   menu_items=None)
