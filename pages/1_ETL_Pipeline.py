38780b86feb8be94c95218114d86d80a0c2f1dc9
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2024-01-30 22:31:46 +0800

    Update 1_ETL_Pipeline.py

--- pages/1_ETL_Pipeline.py
+++ pages/1_ETL_Pipeline.py
@@ -56,32 +56,33 @@ excel_file = col3.file_uploader("Choose a file", type = 'xlsx')
 if excel_file is not None and block_type is not None and year_choice is not None:
     with st.spinner('Processing...'):
         try:
-            processed_data = process_lga_data(block_type, excel_file, year_choice)
+            if excel_file.name.split(".")[-1] == 'xlsx':
+                processed_data = process_lga_data(block_type, excel_file, year_choice)
 
-            # st.dataframe has a default comma in its values, to remove it:
-            s = processed_data.style.format({"Year": lambda x: '{:.0f}'.format(x)})
+                # st.dataframe has a default comma in their values, to remove it:
+                s = processed_data.style.format({"Year": lambda x: '{:.0f}'.format(x)})
+                # Display the dataframe with formatted numeric columns
+                st.subheader(f"{data_desc[block_type]}", divider='grey')
+                st.dataframe(s)
 
-            # Display the formatted dataframe
-            st.subheader(f"{data_desc[block_type]}", divider='grey')
-            st.dataframe(s)
+                st.subheader(f"Load and Explore the Transformed {block_type}", divider='grey')
+                st.write("""
+                Load and explore the transformed data block using Pygwalker. This interactive exploration 
+                interface allows us to build charts by simply dragging and dropping the desired fields. 
+                This hands-on approach facilitates a deeper understanding of the data and aids in the 
+                discovery of valuable insights. 
+                For more information on how to use Pygwalker, please refer to the official [guides on visualizing](https://docs.kanaries.net/graphic-walker/data-viz/create-data-viz).
+                """)
 
-            st.subheader(f"Load and Explore the Transformed {block_type}", divider='grey')
-            st.write("""
-            Load and explore the transformed data block using Pygwalker. This interactive exploration 
-            interface allows us to build charts by simply dragging and dropping the desired fields. 
-            This hands-on approach facilitates a deeper understanding of the data and aids in the 
-            discovery of valuable insights. 
-            For more information on how to use Pygwalker, please refer to the official 
-            [guides on visualizing](https://docs.kanaries.net/graphic-walker/data-viz/create-data-viz).
-            """)
+                # Generate the HTML using Pygwalker
+                pyg_html = pyg.to_html(processed_data)
 
-            # Generate the HTML using Pygwalker
-            pyg_html = pyg.to_html(processed_data)
+                # Embed the HTML into the Streamlit app
+                components.html(pyg_html, height=1000, scrolling=True)
 
-            # Embed the HTML into the Streamlit app
-            components.html(pyg_html, height=1000, scrolling=True)
-
+            else:
+                st.error("Please upload an excel file (.xlsx) only as that is the standard storage of Kaduna TB cases.")
         except (KeyError, ValueError) as e:
-            st.error(f"Error: {str(e)}. The uploaded Excel file does not adhere to the expected structure "
-                     "for 2023 TB cases. Please refer to the [TB Cases 2023 Excel File](https://drive.google.com/drive/folders/1qeHsngqf-2UQ4uaycoE2ubc1BcBnocy0) "
-                     "for proper formatting. If you need assistance, contact support. 🚨")
+            st.error(f"The uploaded Excel file does not adhere to the expected structure. Please refer to the [TB Cases 2023 Excel File](https://drive.google.com/drive/folders/1qeHsngqf-2UQ4uaycoE2ubc1BcBnocy0) for proper formatting. If you need assistance, contact support.", icon="🚨")
+else:
+    st.warning("Please provide values for 'excel_file', 'block_type', and 'year_choice'.")
